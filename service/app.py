4efb5c06930e3805675e3292c21a3c979f848acb
ivanlohvyn <i.logvyn@gmail.com>
2020-10-28 08:53:59 +0000

    sort scores

--- service/app.py
+++ service/app.py
@@ -1,17 +1,30 @@
-from flask import Flask
-from flask import request
 import json
+from operator import itemgetter
+
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["POST"])
-def get_data(filename="data.json"):
+@app.route("/api/v1/scores/send", methods=["POST", "GET"])
+def write_json(filename="data.json"):
     if request.method == "POST":
         game_data = request.get_json()
         with open(filename, "w") as file:
             json.dump(game_data, file, indent=2, ensure_ascii=False)
-    return '<h1>Players</h1>'
+    return "<h1>Game scores</h1>"
+
+
+@app.route("/api/v1/scores/table", methods=["GET"])
+def static_table():
+    file = open("data.json")
+    rating = json.load(file)
+    for player in rating.values():
+        highscores = sorted(player, key=itemgetter("scores"), reverse=True)
+    print(highscores)
+    file.close()
+    return render_template("scores.html", rating=rating, highscores=highscores)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
