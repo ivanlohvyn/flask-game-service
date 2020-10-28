2ca8515e0b4ffd2f17c3ab000cf176422b0b508e
ivanlohvyn <i.logvyn@gmail.com>
2020-10-28 11:58:04 +0000

    update with rendering sorted datas

--- service/app.py
+++ service/app.py
@@ -6,10 +6,11 @@ from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/api/v1/scores/send", methods=["POST", "GET"])
+@app.route("/api/v1/scores/send", methods=["POST"])
 def write_json(filename="data.json"):
     if request.method == "POST":
         game_data = request.get_json()
+
         with open(filename, "w") as file:
             json.dump(game_data, file, indent=2, ensure_ascii=False)
     return "<h1>Game scores</h1>"
@@ -17,13 +18,11 @@ def write_json(filename="data.json"):
 
 @app.route("/api/v1/scores/table", methods=["GET"])
 def static_table():
-    file = open("data.json")
-    rating = json.load(file)
-    for player in rating.values():
-        highscores = sorted(player, key=itemgetter("scores"), reverse=True)
-    print(highscores)
-    file.close()
-    return render_template("scores.html", rating=rating, highscores=highscores)
+    with open("data.json", "r") as file:
+        rating = json.load(file)
+        for player in rating.values():
+            leaders = sorted(player, key=itemgetter("scores"), reverse=True)
+    return render_template("scores.html", leaders=leaders)
 
 
 if __name__ == "__main__":
