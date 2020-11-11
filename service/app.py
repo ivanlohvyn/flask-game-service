61893bbf592a7f2e47ed37f35a91b2548a453e7b
ivanlohvyn <i.logvyn@gmail.com>
2020-11-11 17:08:53 +0000

    insertion sort algorithm

--- service/app.py
+++ service/app.py
@@ -1,28 +1,28 @@
 import json
-from operator import itemgetter
 
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, request
 
 app = Flask(__name__)
 
+with open("data.json", "r") as file:
+    game_data = json.load(file)
+players_data = game_data["data"]
 
-@app.route("/api/v1/scores/send", methods=["POST"])
-def write_json(filename="data.json"):
+
+@app.route("/api/v1/scores/send", methods=["POST", "GET"])
+def write_player():
     if request.method == "POST":
-        game_data = request.get_json()
-
-        with open(filename, "w") as file:
-            json.dump(game_data, file, indent=2, ensure_ascii=False)
-    return "<h1>Game scores</h1>"
-
-
-@app.route("/api/v1/scores/table", methods=["GET"])
-def static_table():
-    with open("data.json", "r") as file:
-        rating = json.load(file)
-        for player in rating.values():
-            leaders = sorted(player, key=itemgetter("scores"), reverse=True)
-    return render_template("scores.html", leaders=leaders)
+        i = 0
+        new_player = request.get_json()
+        for i, player in enumerate(players_data):
+            if player["scores"] >= new_player["scores"]:
+                break
+        else:
+            i = i + 1
+        players_data.insert(i, new_player)
+        with open("data.json", "w") as file:
+            json.dump(game_data, file, indent=4, ensure_ascii=False)
+    return "Players_scores"
 
 
 if __name__ == "__main__":
